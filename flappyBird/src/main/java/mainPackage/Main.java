b878a3025c60ed21603f131beaf246a93d0a8ec3
Kristijonas Stanelis <kristijonasstanelis@gmail.com>
2024-10-30 16:11:36 +0200

    Main: add symbolic display of the map

--- flappyBird/src/main/java/mainPackage/Main.java
+++ flappyBird/src/main/java/mainPackage/Main.java
@@ -3,25 +3,35 @@ package mainPackage;
 public class Main {
 
 
-    static int mapWidthStart = 0, mapWidthEnd = 10;
-    static int mapLengthStart = 0, mapLengthEnd = 30;
-    static int[][] mapFrame = new int[mapWidthEnd][mapLengthEnd];
+    static int mapWidth = 10;
+    static int mapLength = 50;
+    static int[][] mapFrame = new int[mapWidth][mapLength];
 
 
     public static void formatMap(int[][] mapFrame) {
-        for(int i = 0; i < mapWidthEnd; i++){
-            for(int j = 0; j < mapLengthEnd; j++){
-                if(i == mapWidthStart || i == mapWidthEnd - 1 || j == mapLengthStart || j == mapLengthEnd - 1) {
+        for(int i = 0; i < mapWidth; i++){
+            for(int j = 0; j < mapLength; j++){
+                if( (i == 0 || j == 0) || i == mapWidth - 1 || j == mapLength - 1) {
                     mapFrame[i][j] = 1;
                 }
             }
         }
     }
 
-    public static void printMap(int[][] mapFrame){
-        for(int i = 0; i < mapWidthEnd; i++) {
-            for(int j = 0; j < mapLengthEnd; j++){
-                System.out.print(mapFrame[i][j] + " ");
+    public static void printMap(int[][] mapFrame, int mapType){
+        for(int i = 0; i < mapWidth; i++) {
+            for(int j = 0; j < mapLength; j++){
+
+                if(mapType == 1) {
+                    System.out.print(mapFrame[i][j] + " ");
+                } else if (mapType == 2) {
+                    if(mapFrame[i][j] == 1) {
+                        System.out.print("#" + " ");
+                    } else {
+                        System.out.print("." + " ");
+                    }
+                }
+
             }
             System.out.println();
         }
@@ -29,6 +39,6 @@ public class Main {
 
     public static void main(String[] args) {
         formatMap(mapFrame);
-        printMap(mapFrame);
+        printMap(mapFrame, 2);
     }
 }
